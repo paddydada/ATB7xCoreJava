e0b7c4e8281f6cafd26b86517cd7d5a2afee81b4
Pravin Kadam <pravinkadam234@gmail.com>
2024-07-13 10:06:10 +0530

    change loop

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
+            //loop
             System.out.println("i = " + i);
         }
     }
